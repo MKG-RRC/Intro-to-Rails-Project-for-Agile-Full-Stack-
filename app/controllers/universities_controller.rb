24d6fbdde38f4234f2d13d75fedb0e91916df76e
Mark Kenneth Garcia <mgarcia5@rrc.ca>
2025-11-01 03:26:48 -0500

    feat: add search and pagination to universities

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -68,3 +68,87 @@ class UniversitiesController < ApplicationController
       params.require(:university).permit(:name, :country)
     end
 end
+class UniversitiesController < ApplicationController
+  before_action :set_university, only: %i[ show edit update destroy ]
+
+  # GET /universities or /universities.json
+  def index
+    @universities = University.all
+
+    # 🔍 Simple Search (Feature 4.1)
+    if params[:q].present?
+      @universities = @universities.where("name LIKE ?", "%#{params[:q]}%")
+    end
+
+    # 📋 Order alphabetically
+    @universities = @universities.order(:name)
+
+    # 📄 Pagination using Kaminari (Feature 3.5)
+    @universities = @universities.page(params[:page]).per(10)
+  end
+
+  # GET /universities/1 or /universities/1.json
+  def show
+    # Includes associated Programs (Feature 3.3)
+    @programs = @university.programs.order(:name)
+  end
+
+  # GET /universities/new
+  def new
+    @university = University.new
+  end
+
+  # GET /universities/1/edit
+  def edit
+  end
+
+  # POST /universities or /universities.json
+  def create
+    @university = University.new(university_params)
+
+    respond_to do |format|
+      if @university.save
+        format.html { redirect_to @university, notice: "University was successfully created." }
+        format.json { render :show, status: :created, location: @university }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @university.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /universities/1 or /universities/1.json
+  def update
+    respond_to do |format|
+      if @university.update(university_params)
+        format.html { redirect_to @university, notice: "University was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @university }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @university.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /universities/1 or /universities/1.json
+  def destroy
+    @university.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to universities_path, notice: "University was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_university
+      @university = University.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def university_params
+      params.require(:university).permit(:name, :country)
+    end
+end
