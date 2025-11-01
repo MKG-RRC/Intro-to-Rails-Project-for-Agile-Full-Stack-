d7c2b39fa39341429bff4e018ed4a8e9a8e8105a
Mark Kenneth Garcia <mgarcia5@rrc.ca>
2025-11-01 03:19:13 -0500

    Third commit: add University and Program scaffolds

---
+++ db/migrate/20251101081839_create_programs.rb
@@ -0,0 +1,11 @@
+class CreatePrograms < ActiveRecord::Migration[7.1]
+  def change
+    create_table :programs do |t|
+      t.string :name
+      t.text :description
+      t.references :university, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
